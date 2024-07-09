009570b4acaabf995912cc853d8497da2775fe26
Lorofer <lev.buben@gmail.com>
2024-07-10 02:14:56 +0300

    доработана адаптивность на главной странице

--- src/components/CollectionCard.vue
+++ src/components/CollectionCard.vue
@@ -40,6 +40,11 @@ export default {
     padding-right: 0;
   }
 }
+@media (max-width: 700px) {
+  .card-container {
+    padding: 18px 0 18px 18px;
+  }
+}
 
 .card-container img{
   object-fit: cover;
@@ -49,15 +54,43 @@ export default {
 @media (max-width: 1140px) {
   .card-container .image-container{
     width: 40%;
-    min-width: 250px;
+    min-width: 300px;
     height: 300px;
+  }
+}
+@media (max-width: 700px) {
+  .card-container .image-container{
+    min-width: auto;
+    height: auto;
+    width: 100%;
+  }
+  .card-container img{
+    object-fit: contain;
+  }
+}
+@media (max-width: 576px) {
+  .card-container .image-container{
+    max-width: 218px;
   }
 }
 
 @media (max-width: 1140px) {
   .card-container .text-container{
-    width: 364px;
+    max-width: 396px;
+    min-width: 312px;
     margin: auto;
+    padding: 0 16px;
+  }
+}
+@media (max-width: 620px) {
+  .card-container .text-container{
+    min-width: 280px;
+  }
+}
+@media (max-width: 576px) {
+  .card-container .text-container{
+    max-width: 280px;
+    min-width: 220px;
   }
 }
 .card-container h2{
@@ -76,6 +109,16 @@ export default {
   .card-container p{
     margin-bottom: 24px;
     font-size: 18px;
+  }
+}
+@media (max-width: 620px) {
+  .card-container h2{
+    margin: 0 0 18px;
+    font-size: 24px;
+  }
+  .card-container p{
+    margin-bottom: 18px;
+    font-size: 16px;
   }
 }
 </style>
